6faed354b324f7916bb9cc429a57d0ead7e61327
1n4mat1k <90967147+1n4mat1k@users.noreply.github.com>
2023-12-06 20:06:15 +0300

    version 1.0

--- login.php
+++ login.php
@@ -57,7 +57,7 @@
 
     <title class="login-title">Войти</title>
 
-    <link rel="stylesheet" type="text/css" href="css/login.css">
+    <link rel="stylesheet" type="text/css" href="css/login_style.css">
     <link rel="stylesheet" type="text/css" href="css/header_style.css">
     <link rel="stylesheet" type="text/css" href="css/footer_style.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
