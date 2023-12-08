0c274a052a28f2ead1c36b25f9fa7eba0d10d1d4
1n4mat1k <90967147+1n4mat1k@users.noreply.github.com>
2023-12-08 23:50:35 +0300

    version 2.0

--- admin_panel.php
+++ admin_panel.php
@@ -1,34 +1,11 @@
-<!-- <php 
-   if (!empty($_POST['subject'])){
-      echo $_POST['subject'];
-    } else{
-      echo "Пусто";
-    } 
-?>
-
-<form name="form" action="" method="POST">
-    <input type="text" name="subject" id="subject" value="11111">
-    <input type="submit">
-</form> -->
-
-
-
-
-
-
-
-
-
-
-
 
 <!-- Проверка нажата ли кнопка "Сохранить" -->
 <?php
   if(isset($_POST['save']) && !empty($_POST['ID']) && !empty($_POST['Name']) && !empty($_POST['Price']) && !empty($_POST['Year']) 
                           && !empty($_POST['Mileage']) && !empty($_POST['Volume']) && !empty($_POST['Power']) && !empty($_POST['Engine_Type']) 
-                          && !empty($_POST['Transmission']) && !empty($_POST['Configuration']) ){
-  // if (isset($_POST['save'])){
-    echo "Есть подключение к БД";
+                          && !empty($_POST['Transmission']) && !empty($_POST['Configuration']) && !empty($_POST['Pic1']) && !empty($_POST['Pic2'])
+                          && !empty($_POST['Pic3']) && !empty($_POST['Pic4']) && !empty($_POST['Pic5']) ){
+    
     $db_host = 'localhost';
     $db_user = 'root';
     $db_pass = ''; 
@@ -37,92 +14,49 @@
     $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 
     if (!$conn) {
-      die('Ошибка подключения к базе данных: ' . mysqli_connect_error());
+      die('Ошибка подключения к базе данных:' . mysqli_connect_error());
     }
-
-    /*$query = "UPDATE CarInfo SET [ID] = $_POST['ID'], SET [Name] = $_POST['Name'], SET [Price] = $_POST['Price'], SET [Year] = $_POST['Year'], 
-              SET [Mileage] = $_POST['Mileage'], SET [Volume] = $_POST['Volume'], SET [Power] = $_POST['Power'], SET [Engine_Type] = $_POST['Engine_Type'], 
-              SET [Transmission] = $_POST['Transmission'], SET [Configuration] = $_POST['Configuration'], WHERE ID = $_POST['ID']";*/
-
-
-
-    /*$query = "UPDATE CarInfo SET ID = $_POST['ID'], Name = $_POST['Name'], Price = $_POST['Price'], Year = $_POST['Year'], 
-    Mileage = $_POST['Mileage'], Volume = $_POST['Volume'], Power = $_POST['Power'], Engine_Type = $_POST['Engine_Type'], 
-    Transmission = $_POST['Transmission'], Configuration = $_POST['Configuration'] WHERE ID = $_POST['ID']";*/
-
-
-
-
-
-
-
-
-    // РАБОТАЕТ !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    /*$query = "UPDATE CarInfo SET ID = 9, Name = 'Porsch', Price = 100000, Year = 2000, 
-    Mileage = 100000, Volume = 3.5, Power = 450, Engine_Type = 'Бензин', 
-    Transmission = 'АКПП', Configuration = 'Base' WHERE ID = 9";*/
-
-
-
-
-
-
-
-
-
-    /*$ID = 9;
-    $Name = "Pors";
-    $Price = 111111;
-    $Year = 2003;
-    $Mileage = 111;
-    $Volume = 3.5;
-    $Power = 450;
-    $Engine_Type = "Бензин";
-    $Transmission = "АКПП";
-    $Configuration = "База";*/
-
-    // Работает !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    /*$ID1 = 9;
-    $Name1 = 'Pors';
-    $Price1 = 111111;
-    $Year1 = 2003;
-    $Mileage1 = 111;
-    $Volume1 = 3.5;
-    $Power1 = 450;
-    $Engine_Type1 = 'Бензин';
-    $Transmission1 = 'АКПП';
-    $Configuration1 = 'База';
-    $ID1 = 9;
-    $query = "UPDATE CarInfo SET ID = $ID1, Name = '$Name1', Price = $Price1, Year = $Year1, 
-    Mileage = $Mileage1, Volume = $Volume1, Power = $Power1, Engine_Type = '$Engine_Type1', 
-    Transmission = '$Transmission1', Configuration = '$Configuration1' WHERE ID = $ID1";*/
+    else{
+      echo "Есть подключение к БД <br />";
+    }
 
     $id = $_POST['ID'];
-    $ID1 = $_POST['ID'];
-    $Name1 = $_POST['Name'];
-    $Price1 = $_POST['Price'];
-    $Year1 = $_POST['Year'];
-    $Mileage1 = $_POST['Mileage'];
-    $Volume1 = $_POST['Volume'];
-    $Power1 = $_POST['Power'];
-    $Engine_Type1 = $_POST['Engine_Type'];
-    $Transmission1 = $_POST['Transmission'];
-    $Configuration1 = $_POST['Configuration'];
-    $query = "UPDATE CarInfo SET ID = $ID1, Name = '$Name1', Price = $Price1, Year = $Year1, 
-    Mileage = $Mileage1, Volume = $Volume1, Power = $Power1, Engine_Type = '$Engine_Type1', 
-    Transmission = '$Transmission1', Configuration = '$Configuration1' WHERE ID = $ID1";
+    $ID = $_POST['ID'];
+    $Name = $_POST['Name'];
+    $Price = $_POST['Price'];
+    $Year = $_POST['Year'];
+    $Mileage = $_POST['Mileage'];
+    $Volume = $_POST['Volume'];
+    $Power = $_POST['Power'];
+    $Engine_Type = $_POST['Engine_Type'];
+    $Transmission = $_POST['Transmission'];
+    $Configuration = $_POST['Configuration'];
 
+    $query = "UPDATE CarInfo SET ID = $ID, Name = '$Name', Price = $Price, Year = $Year, 
+    Mileage = $Mileage, Volume = $Volume, Power = $Power, Engine_Type = '$Engine_Type', 
+    Transmission = '$Transmission', Configuration = '$Configuration' WHERE ID = $ID";
 
-
-    /*$query = "UPDATE CarInfo SET ID = " . $_POST['ID'] . ", Name = " . Porsche . ", Price = " . $_POST['Price'] . ", Year = " . $_POST['Year'] . ", 
-    Mileage = " . $_POST['Mileage'] . ", Volume = " . $_POST['Volume'] . ", Power = " . $_POST['Power'] . ", Engine_Type = " . $_POST['Engine_Type'] . ", 
-    Transmission = " . $_POST['Transmission'] . ", Configuration = " . $_POST['Configuration'] . " WHERE ID = " . $_POST['ID'] . ""; */
-  
     if ($conn->query($query)){
-      echo "Данные успешно обновлены";
+      echo "Данные успешно обновлены в таблице CarInfo <br />";
     }
     else {
-      echo "Ошибка, данные не добавлены";
+      echo "Ошибка, данные не обновлены в таблице CarInfo <br />";
+    }
+
+
+    $Pic1 = $_POST['Pic1'];
+    $Pic2 = $_POST['Pic2'];
+    $Pic3 = $_POST['Pic3'];
+    $Pic4 = $_POST['Pic4'];
+    $Pic5 = $_POST['Pic5'];
+    $query = "UPDATE CarPictures SET CarID = $ID, Pic1 = '$Pic1', 
+    Pic2 = '$Pic2', Pic3 = '$Pic3', Pic4 = '$Pic4', Pic5 = '$Pic5' WHERE CarID = $ID";
+
+    if ($conn->query($query)){
+      echo "Данные успешно обновлены в таблице CarPictures <br />";
+    }
+    else {
+      echo "Ошибка, данные не обновлены в таблице CarPictures <br />";
     }
 
     $conn->close(); 
@@ -153,9 +87,12 @@
     if (!$conn) {
       die('Ошибка подключения к базе данных: ' . mysqli_connect_error());
     }
+    else{
+      echo "Есть подключение к БД <br />";
+    }
 
     $post_id = $_POST["ID"];
-    $query = "SELECT * FROM CarInfo WHERE ID = $post_id";
+    $query = "SELECT * FROM CarInfo ci JOIN CarPictures cp ON ci.ID = cp.carID WHERE ci.ID = $post_id";
 
     $result = $conn->query($query);
     if ($result->num_rows > 0){
@@ -170,15 +107,16 @@
         $engine_type = $row['Engine_Type'];
         $transmission = $row['Transmission'];
         $configuration = $row['Configuration'];
-        // $pic1 = $row['Pic1'];
-        // $pic2 = $row['Pic2'];
-        // $pic3 = $row['Pic3'];
-        // $pic4 = $row['Pic4'];
-        // $pic5 = $row['Pic5'];
+        $pic1 = $row['Pic1'];
+        $pic2 = $row['Pic2'];
+        $pic3 = $row['Pic3'];
+        $pic4 = $row['Pic4'];
+        $pic5 = $row['Pic5'];
       }
+      echo "Автомобиль был найден <br />";
     }
     else{
-      echo "0 Rows was found";
+      echo "Автомобиль не был найден";
     }
     $conn->close();  
   }
@@ -203,20 +141,18 @@
       die('Ошибка подключения к базе данных: ' . mysqli_connect_error());
     }
     else{
-      echo "<div>Есть подключение к БД</div>";
+      echo "Есть подключение к БД <br />";
     }
 
     $post_id = $_POST["ID"];
     $query = "DELETE FROM CarInfo WHERE ID = $post_id";
 
     if ($conn->query($query)){
-      echo "Данные успешно удалены";
+      echo "Данные успешно удалены <br />";
     }
     else {
-      echo "Ошибка, данные не удалены";
+      echo "Ошибка, данные не удалены <br />";
     }
-
-
     $conn->close(); 
   }
 ?>
@@ -244,60 +180,60 @@
       </tr>
       <tr>
         <td>Name:</td>
-        <td><input type="text" name="Name" value="<?php if (!empty($name)) echo $name; else echo '' ?>"></td>
+        <td><input type="text" name="Name" value="<?php if (!empty($name)) echo $name; elseif(!empty($Name)) echo $Name; else echo '' ?>"></td>
       </tr>
       <tr>
         <td>Price:</td>
-        <td><input type="text" name="Price" value="<?php if (!empty($price)) echo $price; else echo '' ?>"></td>
+        <td><input type="text" name="Price" value="<?php if (!empty($price)) echo $price; elseif(!empty($Price)) echo $Price; else echo '' ?>"></td>
       </tr>
       <tr>
         <td>Year:</td>
-        <td><input type="text" name="Year" value="<?php if (!empty($year)) echo $year; else echo '' ?>"></td>
+        <td><input type="text" name="Year" value="<?php if (!empty($year)) echo $year; elseif(!empty($Year)) echo $Year; else echo '' ?>"></td>
       </tr>
       <tr>
         <td>Mileage:</td>
-        <td><input type="text" name="Mileage" value="<?php if (!empty($mileage)) echo $mileage; else echo '' ?>"></td>
+        <td><input type="text" name="Mileage" value="<?php if (!empty($mileage)) echo $mileage; elseif(!empty($Mileage)) echo $Mileage; else echo '' ?>"></td>
       </tr>
       <tr>
         <td>Volume:</td>
-        <td><input type="text" name="Volume" value="<?php if (!empty($volume)) echo $volume; else echo '' ?>"></td>
+        <td><input type="text" name="Volume" value="<?php if (!empty($volume)) echo $volume; elseif(!empty($Volume)) echo $Volume; else echo '' ?>"></td>
       </tr>
       <tr>
         <td>Power:</td>
-        <td><input type="text" name="Power" value="<?php if (!empty($power)) echo $power; else echo '' ?>"></td>
+        <td><input type="text" name="Power" value="<?php if (!empty($power)) echo $power; elseif(!empty($Power)) echo $Power; else echo '' ?>"></td>
       </tr>
       <tr>
         <td>Engine Type:</td>
-        <td><input type="text" name="Engine_Type" value="<?php if (!empty($engine_type)) echo $engine_type; else echo '' ?>"></td>
+        <td><input type="text" name="Engine_Type" value="<?php if (!empty($engine_type)) echo $engine_type; elseif(!empty($Engine_Type)) echo $Engine_Type; else echo '' ?>"></td>
       </tr>
       <tr>
         <td>Transmission:</td>
-        <td><input type="text" name="Transmission" value="<?php if (!empty($transmission)) echo $transmission; else echo '' ?>"></td>
+        <td><input type="text" name="Transmission" value="<?php if (!empty($transmission)) echo $transmission; elseif(!empty($Transmission)) echo $Transmission; else echo '' ?>"></td>
       </tr>
       <tr>
         <td>Configuration:</td>
-        <td><input type="text" name="Configuration" value="<?php if (!empty($configuration)) echo $configuration; else echo '' ?>"></td>
+        <td><input type="text" name="Configuration" value="<?php if (!empty($configuration)) echo $configuration; elseif(!empty($Configuration)) echo $Configuration; else echo '' ?>"></td>
       </tr>
       <tr>
-        <!-- <td>Picture 1:</td>
-        <td><input type="text" name="pic1" value="<php echo $row_carpictures['Pic1']; ?>"></td>
+      <td>Picture 1:</td>
+        <td><input type="text" name="Pic1" value="<?php if (!empty($pic1)) echo $pic1; elseif(!empty($Pic1)) echo $Pic1; else echo '' ?>"></td>
       </tr>
       <tr>
         <td>Picture 2:</td>
-        <td><input type="text" name="pic2" value="<php echo $row_carpictures['Pic2']; ?>"></td>
+        <td><input type="text" name="Pic2" value="<?php if (!empty($pic2)) echo $pic2; elseif(!empty($Pic2)) echo $Pic2; else echo '' ?>"></td>
       </tr>
       <tr>
         <td>Picture 3:</td>
-        <td><input type="text" name="pic3" value="<php echo $row_carpictures['Pic3']; ?>"></td>
+        <td><input type="text" name="Pic3" value="<?php if (!empty($pic3)) echo $pic3; elseif(!empty($Pic3)) echo $Pic3; else echo '' ?>"></td>
       </tr>
       <tr>
         <td>Picture 4:</td>
-        <td><input type="text" name="pic4" value="<php echo $row_carpictures['Pic4']; ?>"></td>
+        <td><input type="text" name="Pic4" value="<?php if (!empty($pic4)) echo $pic4; elseif(!empty($Pic4)) echo $Pic4; else echo '' ?>"></td>
       </tr>
       <tr>
         <td>Picture 5:</td>
-        <td><input type="text" name="pic5" value="<php echo $row_carpictures['Pic5']; ?>"></td>
-      </tr> -->
+        <td><input type="text" name="Pic5" value="<?php if (!empty($pic5)) echo $pic5; elseif(!empty($Pic5)) echo $Pic5; else echo '' ?>"></td>
+      </tr>
       <tr>
         <td></td>
         <td>
@@ -308,87 +244,5 @@
       </tr>
     </table>
   </form>
-
-
-
-
-
-
-
-  <!-- <form>
-  <input type="hidden" name="id" value="1">
-    <table>
-      <tr>
-        <td>ID:</td>
-        <td>1</td>
-      </tr>
-      <tr>
-        <td>Name:</td>
-        <td><input type="text" name="name" value="merc"></td>
-      </tr>
-      <tr>
-        <td>Price:</td>
-        <td><input type="text" name="price" value="123000"></td>
-      </tr>
-      <tr>
-        <td>Year:</td>
-        <td><input type="text" name="year" value="2010"></td>
-      </tr>
-      <tr>
-        <td>Mileage:</td>
-        <td><input type="text" name="mileage" value="10000"></td>
-      </tr>
-      <tr>
-        <td>Volume:</td>
-        <td><input type="text" name="volume" value="3.0"></td>
-      </tr>
-      <tr>
-        <td>Power:</td>
-        <td><input type="text" name="power" value="200 лс"></td>
-      </tr>
-      <tr>
-        <td>Engine Type:</td>
-        <td><input type="text" name="engine_type" value="Бензин"></td>
-      </tr>
-      <tr>
-        <td>Transmission:</td>
-        <td><input type="text" name="transmission" value="АКПП"></td>
-      </tr>
-      <tr>
-        <td>Configuration:</td>
-        <td><input type="text" name="configuration" value="База"></td>
-      </tr>
-      <tr>
-        <td>Picture 1:</td>
-        <td><input type="text" name="pic1" value=""></td>
-      </tr>
-      <tr>
-        <td>Picture 2:</td>
-        <td><input type="text" name="pic2" value=""></td>
-      </tr>
-      <tr>
-        <td>Picture 3:</td>
-        <td><input type="text" name="pic3" value=""></td>
-      </tr>
-      <tr>
-        <td>Picture 4:</td>
-        <td><input type="text" name="pic4" value=""></td>
-      </tr>
-      <tr>
-        <td>Picture 5:</td>
-        <td><input type="text" name="pic5" value=""></td>
-      </tr>
-      <tr>
-        <td></td>
-        <td>
-          <input type="submit" name="save" value="Сохранить">
-          <a href="delete.php?id=1">Удалить запись</a>
-        </td>
-      </tr>
-    </table>
-  </form> -->
-
-  
-  
 </body>
 </html>
